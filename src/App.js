5f24e6d7b3e735009d21116d188547b1903784f6
Baejita <homokoro@gmail.com>
2024-04-09 20:32:37 +0700

    hero 1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from "react";
 import logoMFP from "./logoMFPWhite.png";
+import Hero from "./components/hero";
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <div>
-        <img src={logoMFP} alt="logo" className="logo" />
-      </div>
-      <ul className="nav-links">
-        <li>
-          <a href="#">แจ้งปัญหา/รับเรื่องร้องเรียน</a>
-        </li>
-        <li>
-          <a href="#">สมัครสมาชิกพรรค</a>
-        </li>
-        <li>
-          <a href="#">อาสาก้าวไกลอยุธยา</a>
-        </li>
-        <li>
-          <a href="#">ติดตามข้อมูลข่าวสาร</a>
-        </li>
-      </ul>
-      <div>
-        <button className="button" type="charin">
-          สส.ชริน วงศ์พันธ์เที่ยง
-        </button>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div>
+          <img src={logoMFP} alt="logo" className="logo" />
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="#">แจ้งปัญหา/รับเรื่องร้องเรียน</a>
+          </li>
+          <li>
+            <a href="#">สมัครสมาชิกพรรค</a>
+          </li>
+          <li>
+            <a href="#">อาสาก้าวไกลอยุธยา</a>
+          </li>
+          <li>
+            <a href="#">ติดตามข้อมูลข่าวสาร</a>
+          </li>
+        </ul>
+        <div>
+          <button className="button" type="charin">
+            สส.ชริน วงศ์พันธ์เที่ยง
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
---
+++ src/components/hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <div className="hero-container">
+        <h1>Hero</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
