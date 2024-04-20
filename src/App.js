6961a59b8c4675e8f871dd0b9e074d053a78c8b6
Baejita <homokoro@gmail.com>
2024-04-20 16:51:44 +0700

    section Atitude

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar.js";
 import Hero from "./components/hero";
+import AttitudeSpeech from "./components/AttitudeSpeech.js";
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <AttitudeSpeech />
     </>
   );
 };
